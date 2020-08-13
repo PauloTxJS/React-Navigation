9ab1ebed9cee79e83f7e4cb2e15c1352180acc34
Paulo Teixeira <paulo.teixeira541@gmail.com>
2020-08-13 15:59:30 -0300

    Navegação Stack parte 5

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -18,7 +18,7 @@ export default props => (
                 ?   <Button 
                         title="Avançar"
                         onPress={() => {
-                            props.navigation.navigate(props.avancar)
+                            props.navigation.push(props.avancar)
                         }}
                     />
                 : false
